e0423f3fb3fbc168aaf14c94e598a11c65e88aca
Alexis <alexisvt@gmail.com>
2017-04-12 12:08:19 -0600

    Adding React Components to the app

--- swipe-component/main.js
+++ swipe-component/main.js
@@ -1,12 +1,14 @@
+// @flow
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Deck from './src/Deck';
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Deck></Deck>
       </View>
     );
   }
@@ -15,9 +17,7 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
 
