b73fdc07999cf1b422406c02913a4957784bf39f
Alexis <alexisvt@gmail.com>
2017-04-12 19:07:43 -0600

    adding gestures

--- swipe-component/src/Deck.js
+++ swipe-component/src/Deck.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 interface IDeckProps {
   renderCard(item: Object): any;
@@ -8,7 +8,16 @@ interface IDeckProps {
 }
 
 class Deck extends Component {
+
   props: IDeckProps;
+  position = new Animated.ValueXY();
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, gesture) => {
+      this.position.setValue({ x: gesture.dx, y: gesture.dy });
+    },
+    onPanResponderRelease: () => { }
+  });
 
   renderCards() {
     return this.props.data.map(item => this.props.renderCard(item));
@@ -16,9 +25,12 @@ class Deck extends Component {
 
   render() {
     return (
-      <View>
+      <Animated.View
+        style={this.position.getLayout()}
+        {...this.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
     );
   }
 }
